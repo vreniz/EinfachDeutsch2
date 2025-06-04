80db5ddc73114f212c59b1b2105bad5c77ee33aa
Aiker Ary Acosta Cantillo <aikerary@gmail.com>
2025-06-04 00:15:14 -0500

    refactor: Update OverallProgress to remove ExpBar props, aligning with recent changes to use UserContext for progress

--- src/pages/OVERALL_PROGRESS/OverallProgress.tsx
+++ src/pages/OVERALL_PROGRESS/OverallProgress.tsx
@@ -64,7 +64,7 @@ export default function OverallProgress() {
 
             {/* Experience Bar Section */}
             <div className="mb-8">
-              <ExpBar level="A1" />
+              <ExpBar />
             </div>
 
             {/* Progress Stats */}
